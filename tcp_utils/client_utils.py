0c9e686bfafcf95c9ba416f412a624ee122a7453
Karol Marszałek <kmarszalek1995@gmail.com>
2023-10-07 13:06:28 +0200

    fixing communication to agv

--- tcp_utils/client_utils.py
+++ tcp_utils/client_utils.py
@@ -13,7 +13,8 @@ class TcpClient:
             ready = select.select([s], [], [], 0.25)
             if ready[0]:
                 return s.recv(1024)
-            return bytes()
+            no_response_mark = 200
+            return no_response_mark.to_bytes(1, 'big')
 
     def sendDataToServer(self, data):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
