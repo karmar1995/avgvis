b00d9310acf828379a8c18791315736300535093
Karol Marszałek <kmarszalek1995@gmail.com>
2024-02-11 15:15:34 +0100

    deprecated code cleanup

--- simulation/core/composition_root.py
+++ simulation/core/composition_root.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
 from simulation.core.system_builder import SystemBuilder
-from simulation.core.paths_controller import PathsController
 from simulation.core.tasks_queue import TasksQueue
 from simulation.core.tasks_scheduler import TasksScheduler
 from simulation.core.job_executors_manager import JobExecutorsManager
-from simulation.core.simulated_annealing_traverser2 import SimulatedAnnealingTraverser
-from simulation.core.genetic_algorithm_traverser2 import GeneticAlgorithmTraverser
+from simulation.core.simulated_annealing_traverser import SimulatedAnnealingTraverser
+from simulation.core.genetic_algorithm_traverser import GeneticAlgorithmTraverser
 from simulation.core.queue_optimizer import QueueOptimizer
 
 
@@ -36,9 +35,6 @@ class CompositionRoot:
         self.__system = systemBuilder.system()
         self.__executorsManager = JobExecutorsManager(taskExecutorsManager=dependencies['taskExecutorsManager'])
         self.__executorsManager.createExecutors()
-        self.__pathsController = PathsController(system=self.__system,
-                                                 agentsFactory=dependencies['agentsFactory'],
-                                                 simulation=dependencies['simulation'])
         self.__queueOptimizer = QueueOptimizer(system=self.__system,
                                                agentsFactory=dependencies['agentsFactory'],
                                                simulation=dependencies['simulation'],
