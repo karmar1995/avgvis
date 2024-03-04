0178a6018b5a32418d30ac73b13382e546e3790f
Karol Marszałek <kmarszalek1995@gmail.com>
2024-03-04 11:46:27 +0100

    cleanup of temporary files use

--- main/appMain.py
+++ main/appMain.py
@@ -3,12 +3,13 @@ from business_rules.composition_root import CompositionRoot, DataInterfaces
 from data_access.opc_data_access import OpcClientFactory
 from data_access.fake_opc_data_access import FakeOpcClientFactory
 from storage.configuration_storage import ConfigurationInJson
+from storage.filesystem import Filesystem
 from PyQt6.QtWidgets import QApplication
 from view.mainframe import Mainframe
 
 fakeOpcDataAccess = FakeOpcClientFactory()
 opcDataAccess = OpcClientFactory()
-configurationDataAccess = ConfigurationInJson()
+configurationDataAccess = ConfigurationInJson(Filesystem())
 dataInterfaces = DataInterfaces(opcDataAccess=opcDataAccess,
                                 configurationDataAccess=configurationDataAccess,
                                 fakeOpcDataAccess=fakeOpcDataAccess
