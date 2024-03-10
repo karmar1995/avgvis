c49d91132fb48ddd7f80ef645136b63b2e0a5b3c
Karol Marszałek <kmarszalek1995@gmail.com>
2024-03-10 10:44:39 +0100

    backup simulation main

--- simulation/main.py
+++ simulation/main.py
@@ -1,10 +1,10 @@
 from simulation.experiments.executable_experiments.agv_random_task_scheduling import run
 from simulation.experiments_utils.test_graphs_builders import *
 
-#run(tasksNumber=100, agvsNumber=25, stationsNumber=40, graphBuilderClass=DebugGraphBuilder, subdirectory='debug')
-#run(tasksNumber=100, agvsNumber=50, stationsNumber=30, graphBuilderClass=VeryLongServiceTimeFullGraphBuilder, subdirectory='long_service_many_agvs')
-#run(tasksNumber=100, agvsNumber=12, stationsNumber=20, graphBuilderClass=LongServiceTimeFullGraphBuilder, subdirectory='long_service_few_agvs')
-#run(tasksNumber=100, agvsNumber=50, stationsNumber=10, graphBuilderClass=ShortServiceTimeFullGraphBuilder, subdirectory='short_service_many_agvs')
+run(tasksNumber=100, agvsNumber=25, stationsNumber=40, graphBuilderClass=DebugGraphBuilder, subdirectory='debug')
+run(tasksNumber=100, agvsNumber=50, stationsNumber=30, graphBuilderClass=VeryLongServiceTimeFullGraphBuilder, subdirectory='long_service_many_agvs')
+run(tasksNumber=100, agvsNumber=12, stationsNumber=20, graphBuilderClass=LongServiceTimeFullGraphBuilder, subdirectory='long_service_few_agvs')
+run(tasksNumber=100, agvsNumber=50, stationsNumber=10, graphBuilderClass=ShortServiceTimeFullGraphBuilder, subdirectory='short_service_many_agvs')
 
 ShortServiceTimeFullGraphWithBranchesBuilder.branches = 1
 run(tasksNumber=100, agvsNumber=50, stationsNumber=10, graphBuilderClass=ShortServiceTimeFullGraphWithBranchesBuilder, subdirectory='short_service_many_agvs_with_branches_k1')
