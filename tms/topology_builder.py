7da49ab034f2ae09f275a1f400815543b9dff276
Karol Marszałek <kmarszalek1995@gmail.com>
2024-03-10 13:53:18 +0100

    ensuring graph continuity

--- tms/topology_builder.py
+++ tms/topology_builder.py
@@ -10,8 +10,16 @@ class TopologyBuilder:
 
     def build(self, systemBuilder):
 
-        for nodeDescription in self.__ds.nodesDescriptions():
+        nodesIndices = []
+        nodesDescriptions = self.__ds.nodesDescriptions()
+        for nodeDescription in nodesDescriptions:
+            nodesIndices.append(nodeDescription.index)
             systemBuilder.addVertex(Vertex(node=Node(env=self.__env, serviceTime=nodeDescription.serviceTime, index=nodeDescription.index), name='unused'))
+
+        maxIndex = max(nodesIndices)
+        for i in range(0, maxIndex):
+            if i not in nodesIndices:
+                systemBuilder.addVertex(Vertex(node=Node(env=self.__env, serviceTime=0, index=i), name='gapFillingNode'))
 
         for edgeDescription in self.__ds.edgesDescriptions():
             systemBuilder.addEdge(Edge(source=edgeDescription.firstNode, target=edgeDescription.secondNode, weight=edgeDescription.transitionTime, name='unused'))
